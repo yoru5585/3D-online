9bb70674f28d53480ae2d82fda2cc7f6715bfeab
yoru <96826783+yoru5585@users.noreply.github.com>
2024-05-15 17:40:47 +0900

    プレイヤーの名前がカメラに向くようにしてたのをもとに戻した

--- 3D-PUN2/Assets/Scripts/NetworkControl/PlayerAvatarView.cs
+++ 3D-PUN2/Assets/Scripts/NetworkControl/PlayerAvatarView.cs
@@ -6,25 +6,13 @@ public class PlayerAvatarView : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI nameLabel;
 
-    [SerializeField]
-    private Camera myCamera;
-
     private void Start()
     {
-        if (myCamera == null)
-        {
-            myCamera = Camera.main;
-        }
+        
     }
     // プレイヤー名をテキストに設定する
     public void SetNickName(string nickName)
     {
         nameLabel.text = nickName;
-    }
-
-    private void LateUpdate()
-    {
-        // プレイヤー名のテキストを、常にカメラの正面向きにする
-        nameLabel.transform.rotation = myCamera.transform.rotation;
     }
 }
