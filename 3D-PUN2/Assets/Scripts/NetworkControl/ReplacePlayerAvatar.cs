df1153a4474adb26c9fc527b2496f9f2da6fc19d
yoru <yoru06460@gmail.com>
2024-05-15 17:31:10 +0900

    a

--- 3D-PUN2/Assets/Scripts/NetworkControl/ReplacePlayerAvatar.cs
+++ 3D-PUN2/Assets/Scripts/NetworkControl/ReplacePlayerAvatar.cs
@@ -9,6 +9,8 @@ public class ReplacePlayerAvatar : MonoBehaviour
 {
     //リストの順にプレイヤー１、プレイヤー２・・・と置き換わります。
     [SerializeField] List<GameObject> PlayerAvatarList;
+
+    [SerializeField] GameObject Camera;
     // Start is called before the first frame update
 
     private void Start()
@@ -21,9 +23,13 @@ public class ReplacePlayerAvatar : MonoBehaviour
     {
         GameObject myAvatar = PhotonNetwork.Instantiate("PlayerAvatar", Vector3.zero, Quaternion.identity);
         myAvatar.transform.position = PlayerAvatarList[Resources.Load<PlayerInfo_s>("PlayerInfo").playerID].transform.position;
+        Camera.transform.parent = myAvatar.transform;
+        Camera.transform.position = new Vector3(0, 2.4f, 0);
         foreach (GameObject obj in PlayerAvatarList)
         {
             Destroy(obj);
         }
     }
+
+
 }
